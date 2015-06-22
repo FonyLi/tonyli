18e8c715b7f80ce7d4f5edc4645d8bc428f296ee
Tony <ifreal@hotmail.com>
2015-06-22 19:41:11 +1000

    need to run under bash but not sh
    do not run it using "sh xxx.sh"
    should run like this: 
    "chmod +x xxx.sh
    ./xxx.sh
    "

--- license_checker.sh
+++ license_checker.sh
@@ -1,14 +1,20 @@
-
 #!/bin/bash
-#might need to add oauth to avoid running into github rate limiting quotas
 
-rm -rf ./*.yml ./*.txt license
+#clear folder
+rm -rf ./*.yml ./*.txt ./*.csv license
+mkdir license
 
-curl https://api.github.com/orgs/cloudfoundry/repos?page=[1-2] >> repos.yml
-curl https://api.github.com/orgs/cloudfoundry-incubator/repos?page=[1-2] >> incubator_repos.yml
+#add head to the csv file
+CSV_FILE="license_table.csv";
+echo "Repository name,Does LICENSE file exist,Apache License v2,License info,URL" >> $CSV_FILE
+
+APACHE_LICENSE="Apache License, Version 2.0"
+MESSAGE_LICENSE_TEXT_FOUND="found license text"
+
+curl https://api.github.com/orgs/cloudfoundry/repos?page=[1] >> repos.yml
 
 #get gits url
-echo "get gits url"
+echo "get gits url";
 REPOS=$(cat repos* *incu* | grep git_url | cut -f 4 -d'"');
 for REPO in $REPOS;
 do
@@ -16,58 +22,101 @@ do
   echo $REPO >> git.txt;
 done;
 
-#get licenses url
-echo "get lecenses url";
+# check if a license file exists
+echo "check license files"
+
+COMPOENT=""
+LICENSE=""
+FILE_NAME=""
+LICENSE_NAME=""
+
 GITS=$(cat git.txt);
 for GIT in $GITS;
-do echo "$GIT" | sed -r 's#git://(.*).git#https://\1/blob/master/LICENSE#g' >> license.txt;
-done;
-
-# check if exist and have keyword "copyright"
-echo "chech license file"
-LICENSES=$(cat license.txt);
-
-mkdir license;
-
-for LICENSE in $LICENSES;
 do
-  #remove head
-  PROJECT_NAME=$( echo $LICENSE | sed -r 's/https:\/\/github.com\/cloudfoundry\///g');
+  COMPONENT=$(echo $GIT | sed -r 's#git://github.com/(.*).git#\1#g');
 
-  #remove tail
-  PROJECT_NAME=$( echo $PROJECT_NAME | sed -r 's/\/blob\/master\/LICENSE//g');
+  echo "start to chech component: $COMPONENT"
+  LICENSE=$(echo "https://github.com/"$COMPONENT"/blob/master")
 
-  #replace / with -
-  PROJECT_NAME=$( echo $PROJECT_NAME | sed -r 's/\//\-/g');
+  LICENSE_NAME=$LICENSE"/LICENSE";
   
-  FILE_NAME="./license/$PROJECT_NAME";
+  FILE_NAME="./license/"$(echo $COMPONENT | sed -r 's/\//\_/g');
 
-  echo "curl from " $LICENSE "and store it to " $FILE_NAME;
+  echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
 
-  curl $LICENSE >> $FILE_NAME;
+  curl $LICENSE_NAME >> $FILE_NAME
 
-  sleep 2;
-
-  if [ -f $FILE_NAME ]; #notice!!! after $FILE_NAME, there must be a space!
+  echo "file size is $(stat -c%s $FILE_NAME )"
+  
+  #if the file size is very small, normally it is "Not Found"..
+  if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ]; #notice!!! after $FILE_NAME, there must be a space!
   then
+    rm $FILE_NAME
+    #check if LICENSE.TXT exist all capital cases, end with txt
+    LICENSE_NAME=$LICENSE"/LICENSE.TXT";
+    echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
+    curl $LICENSE_NAME >> $FILE_NAME
 
-    # TODO: some project has license with different file name such
-    # https://github.com/cloudfoundry/common/blob/master/LICENSE.TXT
-    # and
-    # https://github.com/cloudfoundry/eclipse-integration-cloudfoundry/blob/master/License.txt
-    # we will add new functions to check them
-
-    if grep -q "Not Found" $FILE_NAME; # notice! do not use [ ... ]
+    if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ];
     then
-      rm $FILE_NAME;
-      echo "for project $PROJECT_NAME " $LICENSE " doesn't exist" >> report.txt
-    else
-      echo "$PROJECT_NAME has license. good!";
+      rm $FILE_NAME
+      #check if License.txt exist, capitalise the first chracter, end with txt
+      LICENSE_NAME=$LICENSE"/License.txt";
+      echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
+      curl $LICENSE_NAME >> $FILE_NAME
+
+      if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ];
+      then
+        rm $FILE_NAME
+        #check if license exist, all lower case, end with txt
+        LICENSE_NAME=$LICENSE"/license.txt";
+        echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
+        curl $LICENSE_NAME >> $FILE_NAME
+
+        if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ];
+        then
+          rm $FILE_NAME
+          #check if license exist, all lower case
+          LICENSE_NAME=$LICENSE"/license";
+          echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
+          curl $LICENSE_NAME >> $FILE_NAME
+
+          if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ];
+          then
+            rm $FILE_NAME
+            #check if license exist, all lower case
+            LICENSE_NAME=$LICENSE"/LICENSE.txt";
+            echo "curl from " $LICENSE_NAME "and store it to " $FILE_NAME;
+            curl $LICENSE_NAME >> $FILE_NAME
+          fi
+        fi
+      fi
     fi
+  fi
+
+# It seems to be enough to provide functions above to check the license
+# but if there is any license file with other names, we can add functions to support that in the future.
+
+  #use base name and ignore folder(path) name
+  LICENSE_NAME=$(basename $LICENSE_NAME)
+  
+  if [ ! -f $FILE_NAME ] || [ $(stat -c%s $FILE_NAME ) -lt 25 ];
+  then
+    rm $FILE_NAME;
+    echo "for project $PROJECT_NAME " $LICENSE " doesn't exist" >> report.txt
+    echo "$COMPONENT,-,n,-,$GIT" >> $CSV_FILE
   else
-    echo "$FILE_NAME is missing.. why?";
-    echo "for project $PROJECT_NAME " $LICENSE " doesn't exist" >> report.txt;
+    #check if it includes apache license key word
+    if grep -q "$APACHE_LICENSE" $FILE_NAME
+      then
+      #this component do have a license file with apache license
+      echo "$COMPONENT,$LICENSE_NAME,y,$MESSAGE_LICENSE_TEXT_FOUND,$GIT" >> $CSV_FILE
+      echo "for project $PROJECT_NAME " $LICENSE " found. But it doesn't include Apache license." >> report.txt;
+    else
+      #this component has a license file, but do not include apache license
+      echo "$COMPONENT,$LICENSE_NAME,n,-,$GIT" >> $CSV_FILE
+      echo "for project $PROJECT_NAME " $LICENSE " found. Good" >> report.txt;
+    fi
   fi
 
 done;
-
